2e4725ced0fa4aa87cac41001171a588c74bf973
ehsan saraee <ehsansaraee6570@yahoo.com>
2021-07-05 12:42:10 +0430

    Our Work Scroll

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -15,8 +15,11 @@ import {
    sliderContainer,
    slider,
 } from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
+   const [element, controls] = useScroll();
+   const [element2, controls2] = useScroll();
    return (
       <Work
          variants={pageAnimation}
@@ -44,16 +47,26 @@ const OurWork = () => {
                </Hide>
             </Link>
          </Movie>
-         <Movie>
+         <Movie
+            ref={element}
+            variants={fade}
+            initial="hidden"
+            animate={controls}
+         >
             <h2>The Racer</h2>
-            <div className="line"></div>
+            <motion.div variants={lineAnimation} className="line"></motion.div>
             <Link to="/work/the-racer">
                <img src={theracer} alt="theracer" />
             </Link>
          </Movie>
-         <Movie>
+         <Movie
+            ref={element2}
+            variants={fade}
+            initial="hidden"
+            animate={controls2}
+         >
             <h2>Good Times</h2>
-            <div className="line"></div>
+            <motion.div variants={lineAnimation} className="line"></motion.div>
             <Link to="/work/good-times">
                <img src={goodtimes} alt="goodtimes" />
             </Link>
@@ -72,7 +85,7 @@ const Work = styled(motion.div)`
    }
 `;
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
    padding-bottom: 10rem;
    .line {
       height: 0.2rem;
