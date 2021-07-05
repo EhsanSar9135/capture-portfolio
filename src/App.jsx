2aa514c96a633cbc451277550e6ca32e2cbb169a
ehsan saraee <ehsansaraee6570@yahoo.com>
2021-07-05 06:35:07 +0430

    Page Transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-//Import Nav
+//Import Components
 import Nav from "./components/Nav";
 // Import Style
 import GlobalStyle from "./components/GlobalStyle";
@@ -7,21 +7,26 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
-// Import React-Router
-import { Route, Switch } from "react-router-dom";
 import MovieDetail from "./pages/MovieDetail";
+// Import React-Router
+import { Route, Switch, useLocation } from "react-router-dom";
+// Import Animation
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
+   const location = useLocation();
    return (
       <>
          <GlobalStyle />
          <Nav />
-         <Switch>
-            <Route path="/" exact component={AboutUs} />
-            <Route path="/work" exact component={OurWork} />
-            <Route path="/work/:id" exact component={MovieDetail} />
-            <Route path="/contact" exact component={ContactUs} />
-         </Switch>
+         <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+               <Route path="/" exact component={AboutUs} />
+               <Route path="/work" exact component={OurWork} />
+               <Route path="/work/:id" exact component={MovieDetail} />
+               <Route path="/contact" exact component={ContactUs} />
+            </Switch>
+         </AnimatePresence>
       </>
    );
 };
